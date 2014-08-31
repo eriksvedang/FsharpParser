78ce9e48ee92d27e7074f1214620f0f508a66b57
Erik <erik.svedang@gmail.com>
2014-08-31 12:39:44 +0200

    first

---
+++ Parser/Parser.fs
@@ -0,0 +1,38 @@
+﻿module Parser
+
+let (~%) (s: string) = s.ToCharArray() |> Array.toList
+let charListToStr cs = String.concat "" (List.map (fun c -> c.ToString()) cs)
+
+let ($) a b = a b
+
+// 'r is the type the parser produces, like the AST
+// The function stored in the parser takes a list of chars
+// and returns a list of pairs containing the result and
+// the remaining chars
+type Parser<'r> = Parser of (char list -> ('r * char list) list)
+
+let parse (Parser p) = p
+
+let (>>=) p f = 
+    Parser(fun chars -> 
+        List.concat [for (r, remaining) in parse p chars -> parse (f r) remaining])
+
+let (>>) p q = p >>= fun _ -> q
+
+// Consume nothing, create a result 'r'
+let mreturn r = Parser(fun chars -> [(r, chars)])
+
+let lambda = Parser(fun _ -> [])
+
+// Parse one char unconditionally
+let item = Parser(fun chars -> match chars with
+                               | [] -> []
+                               | (c::cs) -> [(c,cs)])
+
+let sat cond = item >>= fun c -> if cond c then mreturn c else lambda
+
+let ch c = sat ((=) c)
+
+
+
+
---
+++ Parser/Script.fsx
@@ -0,0 +1,51 @@
+﻿#load "Parser.fs"
+open Parser
+
+let x = %"erik"
+
+parse (mreturn %"hej") %"fisk"
+parse item %"fisk"
+parse (item >> item) %"fisk"
+
+parse (item >> item >> (mreturn "FOO")) %"fisk"
+
+parse (ch 'a') %"apa"
+parse (ch 'b') %"apa"
+
+parse ((ch 'a') >> (ch 'p')) %"apa"
+parse ((ch 'a') >> (ch 'ö')) %"apa"
+
+let go = (ch 'g') >> (ch 'o')
+parse go %"go"
+
+'r' + 'a'
+
+let goo =
+    (ch 'g') >>= fun c1 ->
+    (ch 'o') >>= fun c2 ->
+    mreturn (charListToStr [c1;c2])
+
+parse goo %"goo"
+parse goo %"ggo"
+
+
+type Lang = A | B | C
+let lang = (ch 'f') >> mreturn B
+parse lang %"fff"
+parse lang %"ggg"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
